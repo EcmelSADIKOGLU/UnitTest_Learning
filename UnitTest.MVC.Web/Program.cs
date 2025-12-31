372dcc87b34605ee76ba91dbf873f171cee8644e
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2026-01-01 01:45:37 +0300

    Added: Repository + ProductController and Product Views

--- UnitTest.MVC.Web/Program.cs
+++ UnitTest.MVC.Web/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using UnitTest.MVC.Web.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<UnitTestLearningDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"));
+});
 
 var app = builder.Build();
 
---
+++ UnitTest.MVC.Web/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using UnitTest.MVC.Web.Models;
+
+namespace UnitTest.MVC.Web.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly UnitTestLearningDbContext _context;
+        private readonly DbSet<T> _dbSet;   
+        public Repository(UnitTestLearningDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var value = await _dbSet.ToListAsync();
+            return value;
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            var value = await _dbSet.FindAsync([id]);
+            return value;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
